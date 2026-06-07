d8f0bd38574c7131d8fb149af88102273a861a5f
Ikubimu <ikerubidemunoz@gmail.com>
2026-06-07 22:54:07 +0200

    Implement motor driver functionality with encoder support and direction control

--- Core/Inc/Motor.hpp
+++ Core/Inc/Motor.hpp
@@ -16,28 +16,46 @@
 #define MICROSTEP_8     8
 #define MICROSTEP_16   16
 
+#define DIR_CW  0
+#define DIR_CCW 1
+
 class Motor {
 public:
-    Motor(PWM &pwm, ADC &adc, uint32_t adc_channel,
-          DigitalOutput &ms1, DigitalOutput &ms2, DigitalOutput &ms3);
+    static Motor& getInstance(PWM &pwm, ADC &adc, uint32_t adc_channel,
+                              DigitalOutput &ms1, DigitalOutput &ms2, DigitalOutput &ms3,
+                              DigitalOutput &dir);
+    static Motor& getInstance();
+    static uint32_t get_raw_encoder();
+    static float   get_actual_speed();
+    static bool    get_direction();
 
     void    set_speed(float rad_s);
+    void    set_direction(bool cw);
     float   get_position_rad();
+    uint32_t get_position_raw();
     void    set_microstep(uint8_t step);
-    float   get_speed() const;
 
 private:
+    Motor(PWM &pwm, ADC &adc, uint32_t adc_channel,
+          DigitalOutput &ms1, DigitalOutput &ms2, DigitalOutput &ms3,
+          DigitalOutput &dir);
+    ~Motor() = default;
+    Motor(const Motor&) = delete;
+    Motor& operator=(const Motor&) = delete;
+
     PWM          &pwm;
     ADC          &adc;
     uint32_t     adc_channel;
     DigitalOutput &ms1;
     DigitalOutput &ms2;
     DigitalOutput &ms3;
+    DigitalOutput &dir;
 
-    float        current_speed;
     uint8_t      microstep;
 
     void apply_microstep();
+
+    static Motor *s_instance;
 };
 
 #endif
--- Core/Src/Motor.cpp
+++ Core/Src/Motor.cpp
@@ -1,4 +1,8 @@
 #include "Motor.hpp"
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define ENCODER_READ_MS 100
 
 static const uint8_t microstep_table[5][3] = {
     {0, 0, 0},  // MICROSTEP_1  (full)
@@ -8,27 +12,92 @@ static const uint8_t microstep_table[5][3] = {
     {1, 1, 1},  // MICROSTEP_16 (sixteenth)
 };
 
+static volatile uint32_t s_raw_encoder = 0;
+static volatile float   s_position_rad = 0.0f;
+static volatile float   s_actual_speed = 0.0f;
+static volatile bool    s_direction = DIR_CW;
+
+Motor *Motor::s_instance = nullptr;
+
+static void vTaskEncoder(void *pvParameters) {
+    (void)pvParameters;
+    static const float delta_sec = (float)ENCODER_READ_MS * 0.001f;
+    uint32_t prev_raw = Motor::getInstance().get_position_raw();
+    for (;;) {
+        vTaskDelay(pdMS_TO_TICKS(ENCODER_READ_MS));
+        uint32_t curr_raw = Motor::getInstance().get_position_raw();
+        int32_t delta_raw = (int32_t)curr_raw - (int32_t)prev_raw;
+
+        if (s_direction == DIR_CW) {
+            if (delta_raw < 0)
+                delta_raw += 4096;
+        } else {
+            if (delta_raw > 0)
+                delta_raw -= 4096;
+        }
+
+        s_actual_speed = (float)delta_raw * 2.0f * MOTOR_PI / (4095.0f * delta_sec);
+        s_position_rad = (float)curr_raw * 2.0f * MOTOR_PI / 4095.0f;
+        s_raw_encoder = curr_raw;
+        prev_raw = curr_raw;
+    }
+}
+
+Motor& Motor::getInstance(PWM &pwm, ADC &adc, uint32_t adc_channel,
+                          DigitalOutput &ms1, DigitalOutput &ms2, DigitalOutput &ms3,
+                          DigitalOutput &dir) {
+    if (s_instance == nullptr)
+        s_instance = new Motor(pwm, adc, adc_channel, ms1, ms2, ms3, dir);
+    return *s_instance;
+}
+
+Motor& Motor::getInstance() {
+    return *s_instance;
+}
+
+uint32_t Motor::get_raw_encoder() {
+    return s_raw_encoder;
+}
+
+float Motor::get_actual_speed() {
+    return s_actual_speed;
+}
+
+bool Motor::get_direction() {
+    return s_direction;
+}
+
 Motor::Motor(PWM &pwm, ADC &adc, uint32_t adc_channel,
-             DigitalOutput &ms1, DigitalOutput &ms2, DigitalOutput &ms3)
+             DigitalOutput &ms1, DigitalOutput &ms2, DigitalOutput &ms3,
+             DigitalOutput &dir)
     : pwm(pwm), adc(adc), adc_channel(adc_channel),
-      ms1(ms1), ms2(ms2), ms3(ms3),
-      current_speed(0.0f), microstep(MICROSTEP_1)
+      ms1(ms1), ms2(ms2), ms3(ms3), dir(dir),
+      microstep(MICROSTEP_1)
 {
     pwm.set_duty_percent(50.0f);
+    set_direction(DIR_CW);
     apply_microstep();
+    xTaskCreate(vTaskEncoder, "Encoder", 128, NULL, 1, NULL);
 }
 
 void Motor::set_speed(float rad_s) {
     if (rad_s < 0.0f) rad_s = 0.0f;
-    current_speed = rad_s;
     float freq = rad_s * (float)STEPS_PER_REV / (2.0f * MOTOR_PI);
     if (freq < 1.0f) freq = 0.0f;
     pwm.set_freq((uint32_t)freq);
 }
 
+void Motor::set_direction(bool cw) {
+    s_direction = cw;
+    dir.write(cw);
+}
+
 float Motor::get_position_rad() {
-    uint32_t raw = adc.read_channel(adc_channel);
-    return (float)raw * 2.0f * MOTOR_PI / 4095.0f;
+    return s_position_rad;
+}
+
+uint32_t Motor::get_position_raw() {
+    return adc.read_channel(adc_channel);
 }
 
 void Motor::set_microstep(uint8_t step) {
@@ -41,10 +110,6 @@ void Motor::set_microstep(uint8_t step) {
         default:           microstep = 0; break;
     }
     apply_microstep();
-}
-
-float Motor::get_speed() const {
-    return current_speed;
 }
 
 void Motor::apply_microstep() {
