af67af8d2622f57dec92c94d8ae54d2824458f96
Ikubimu <ikerubidemunoz@gmail.com>
2026-06-08 21:36:28 +0200

    fix print in cpp file

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -556,7 +556,7 @@ void Error_Handler(void)
 }
 
 /* USER CODE BEGIN 4 */
-int _write(int fd, char *ptr, int len) {
+extern "C" int _write(int fd, char *ptr, int len) {
     HAL_UART_Transmit(&huart1, (uint8_t *)ptr, len, HAL_MAX_DELAY);
     return len;
 }
