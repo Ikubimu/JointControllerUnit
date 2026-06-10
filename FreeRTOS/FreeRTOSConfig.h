1af40704ec145563a1b62ed63177356f4acd1859
Ikubimu <ikerubidemunoz@gmail.com>
2026-06-10 20:02:01 +0200

    change FreeRTos heap size

--- FreeRTOS/FreeRTOSConfig.h
+++ FreeRTOS/FreeRTOSConfig.h
@@ -10,7 +10,7 @@
 #define configCPU_CLOCK_HZ                      8000000
 #define configMAX_PRIORITIES                    5
 #define configMINIMAL_STACK_SIZE                128
-#define configTOTAL_HEAP_SIZE                   4096
+#define configTOTAL_HEAP_SIZE                   8192
 #define configMAX_TASK_NAME_LEN                 16
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
