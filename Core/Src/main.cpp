d00df1037677537106b5f7f364df24d5a43757ef
Ikubimu <ikerubidemunoz@gmail.com>
2026-06-10 23:37:48 +0200

    fix Task include

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -22,6 +22,8 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "JointController.hpp"
+#include "FreeRTOS.h"
+#include "task.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
